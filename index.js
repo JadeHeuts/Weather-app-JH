37174694bdd03857e9879a1db866bbf1ef3fda96
Jade Heuts <jadeheuts@hotmail.com>
2022-02-27 19:35:01 +0100

    Change image based on the weather API

--- index.js
+++ index.js
@@ -37,6 +37,15 @@ function displayWeatherCondition(response) {
   document.querySelector("#date").innerHTML = formatDate(
     response.data.dt * 1000
   );
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].main);
 }
 
 function search(city) {
